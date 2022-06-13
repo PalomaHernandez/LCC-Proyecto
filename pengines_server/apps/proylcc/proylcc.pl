a95b71b1a359d38c620a5804ee2fc77f8a0291d2
PalomaHernandez <palohernandez01@gmail.com>
2022-06-12 22:33:15 -0300

    prolog comentado

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -133,84 +133,193 @@ flick(Grid, Color,AdyacentesC, FGrid, NewAdyacentesC):-
     setColor(Grid, AdyacentesC, Color, FGrid), !,
     adyCStar(AdyacentesC, FGrid, NewAdyacentesC).
     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% remove(+C, +List1, -List2)
+%
+% C es el elemento a eliminar de la lista.
+% List1 es la lista que contiene el elemento a eliminar .
+% List2 es el resultado de eliminar el elemento C de la lista List1.
+
 remove(_, [], []).
 remove(C, [C|Xs], Xs).
 remove(C, [X|Xs], [X|Zs]):- X \= C, remove(C, Xs, Zs).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  listaSinColor(+Color, -Colors)
+%
+% Color es el color a eliminar de la lista de colores.
+% Colors es el resultado de eliminar el color Color de la lista de colores .
+
 listaSinColor(Color, Colors):-
     remove(Color, [r,v,p,g,b,y], Colors).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  help(+Grid, +AdyacentesC,  +N, -Solucion, -CantAdyacentes
+%
+% Grid es la grilla actual.
+% AdyacentesC es la lista de adyacentesC* actual.
+% N es la profundidad ingresada por el usuario.
+% Solucion es una lista con la mejor secuencia de movidas con profundidad N de la ayuda % optimal.
+% CantAdyacentes es la cantidad de celdas que se capturan al realizar flick con la   
+% secuencia de movidas de la lista Solucion.
+
 help(Grid, AdyacentesC, N, Solucion, CantAdyacentes):-
     N > 0,
-	predicadoOne(Grid,AdyacentesC,N, [], Resultado),
+    helpOne(Grid,AdyacentesC,N, [], Resultado),
     caminosPosibles(Resultado, CaminosPosibles), !,
-    sort(2, @>=, CaminosPosibles, LFSolucionesAux),
-    nth0(0, LFSolucionesAux, Mov),
+    sort(2, @>=, CaminosPosibles, SolucionesAux),
+    nth0(0, SolucionesAux, Mov),
     nth0(1, Mov, Ady),
-    findall(X, (member(X, LFSolucionesAux),nth0(1,X,Y), Y== Ady), LFSoluciones),
-	sort(2, @=<, LFSoluciones, LFSoluciones2),
-    LFSoluciones2=[MejorCamino|_Otros],
+    findall(X, (member(X, SolucionesAux),nth0(1,X,Y), Y== Ady), LFSolucionesAux),
+    sort(2, @=<, LFSolucionesAux, LFSoluciones),
+    LFSoluciones=[MejorCamino|_Otros],
     MejorCamino=[Solucion,CantAdyacentes].
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  caminosPosibles(+Lista1, -Lista2)
+%
+% Lista1 es la lista de listas que contienen una grilla, una lista de movimientos y una lista de % adyacentesC.
+% Lista2 es una lista que se forma a partir de la lista de movimientos y la lista de  
+% adyacentesC deLista1
+
 caminosPosibles([],[]):-!.
 caminosPosibles([L|Ls],[[Movimientos,CantAdy]|Res]):-
-	L= [_Grid, Movimientos, Ady],
-	length(Ady,CantAdy),
-	caminosPosibles(Ls, Res).
+    L= [_Grid, Movimientos, Ady],
+    length(Ady,CantAdy),
+    caminosPosibles(Ls, Res).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  helpOne(+Grid, +AdyacentesC, +N, +Secuencia, -Resultado)
+%
+% Grid es una grilla.
+% AdyacentesC es la lista de celdas capturadas en la grilla Grid.
+% N es la profundidad en la que se encuentra la secuencia de jugadas.
+% Secuencia es una lista de colores.
+% Resultado lista de listas que contiene todas las posibles jugadas incluyendo: grilla, 
+% secuencia de colores y cantidad de celdas capturadas.
 
-predicadoOne(_Grid, _AdyacentesC, 0, _Secuencia, _Resultado):-!.
-predicadoOne(Grid, AdyacentesC, N, Secuencia, Resultado):-
+helpOne(_Grid, _AdyacentesC, 0, _Secuencia, _Resultado):-!.
+helpOne(Grid, AdyacentesC, N, Secuencia, Resultado):-
     N1 is N -1,
     AdyacentesC=[A|_],
     color(Grid, A, Color),
     listaSinColor(Color, Colors),
     caminoPorNivel(Grid, Colors, AdyacentesC, Secuencia, R),
-    predicadoAll(R, N1,Resul),
+    helpAll(R, N1,Resul),
     append(Resul,R,Resultado).
 
-predicadoAll([],_,_):-!.
-predicadoAll(R, N,Resul):-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  helpAll(+R, +N, -Resul)
+%
+% R es una lista de listas que contiene las posibles jugadas incluyendo: grilla, secuencia de % colores y cantidad de celdas capturadas.
+% N es la profundidad en la que se encuentra la secuencia de jugadas.
+% Resul lista de listas que contiene las posibles jugadas que se encuentran a partir de R.
+
+
+helpAll([],_,_):-!.
+helpAll(R, N,Resul):-
     R=[L|Ls],
     L=[NewGrid, NewSecuencia, NewAdyacentesC],
-    predicadoOne(NewGrid, NewAdyacentesC, N,  NewSecuencia, Res),
-    predicadoAll( Ls, N,Re),
+    helpOne(NewGrid, NewAdyacentesC, N,  NewSecuencia, Res),
+    helpAll( Ls, N,Re),
     append(Re,Res,Resul).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  caminoPorNivel(+Grid, +Colors, +AdyacentesC, +Secuencia, -Resultado)
+%
+% Grid es una grilla.
+% Colors es una lista de colores.
+% AdyacentesC es la lista de celdas capturadas en la grilla Grid.
+% Secuencia es una lista de colores.
+% Resultado es una lista de listas, donde cada una de ellas almacena la cantidad de celdas % capturadas, grilla luego de hacer flick de un color, la secuencia de colores actualizada y la % nueva cantidad de celdas capturadas
+
 caminoPorNivel(Grid, Colors, AdyacentesC, Secuencia, Resultado):-
-    findall([NewGrid, NewSecuencia, NewAdyacentesC], (member(C, Colors), flick(Grid, 
-    C,AdyacentesC, NewGrid, NewAdyacentesC), 
+    findall([NewGrid, NewSecuencia, NewAdyacentesC], (member(C, Colors), flick(Grid,
+    C,AdyacentesC, NewGrid, NewAdyacentesC),
     length(NewAdyacentesC,Long1),length(AdyacentesC,Long2),
     Long1 > Long2,
-	append(Secuencia, [C], NewSecuencia)),
+    append(Secuencia, [C], NewSecuencia)),
     Resultado), !.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  helpGreedy(+Grid, +AdyacentesC,  +N, -Solucion, -CantAdyacentes
+%
+% Grid es la grilla actual.
+% AdyacentesC es la lista de adyacentesC* actual.
+% N es la profundidad ingresada por el usuario.
+% Solucion es una lista con la mejor secuencia de movidas con profundidad N de la ayuda % greedy.
+% CantAdyacentes es la cantidad de celdas que se capturan al realizar flick con la   
+% secuencia de movidas de la lista Solucion.
+
 
 helpGreedy(Grid, AdyacentesC, N, Solucion, CantAdyacentes):-
     N > 0,
-	predicadoUno(Grid,AdyacentesC,N,[], Resultado),
-	Resultado = [CantAdyacentes, _NewGrid, Solucion, _NewAdyacentesC].
+    greedyOne(Grid,AdyacentesC,N,[], Resultado),
+    Resultado = [CantAdyacentes, _NewGrid, Solucion, _NewAdyacentesC].
 
-predicadoUno(Grid, AdyacentesC, N, Secuencia, Resultado):-
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  greedyOne(+Grid, +AdyacentesC, +N, +Secuencia, -Resultado)
+%
+% Grid es una grilla.
+% AdyacentesC es la lista de celdas capturadas en la grilla Grid.
+% N es la profundidad en la que se encuentra la secuencia de jugadas.
+% Secuencia es una lista de colores.
+% Resultado lista de listas que contiene la mejor jugada incluyendo su grilla resultante, 
+% la secuencia de colores y la cantidad de celdas que se capturan.
+
+greedyOne(Grid, AdyacentesC, N, Secuencia, Resultado):-
     N1 is N -1,
     AdyacentesC=[A|_],
     color(Grid, A, Color),
     listaSinColor(Color, Colors),
-    caminoPorNivel2(Grid, Colors, AdyacentesC, Secuencia, R),
+    caminoPorNivelAlternativo(Grid, Colors, AdyacentesC, Secuencia, R),
     sort(0, @>=, R, RAux),
-	nth0(0, RAux, Mov),	
-    predicadoTodos(Mov, N1,Resultado), !.
+    nth0(0, RAux, Mov),
+    greedyAll(Mov, N1,Resultado), !.
    
 
-predicadoTodos(R, 0, R):-!.
-predicadoTodos(R, N,Resul):-
-    R=[ _LongitudAdy, NewGrid, NewSecuencia, NewAdyacentesC],
-    predicadoUno(NewGrid, NewAdyacentesC, N,  NewSecuencia, Resul).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  greedyAll(+R, +N, -Resul)
+%
+% R es una lista de listas que contiene la mejor jugada del nivel de profundidad en el que se % encuentra incluyendo su grilla, la secuencia de colores y la cantidad de celdas 
+% capturadas.
+% N es la profundidad en la que se encuentra la secuencia de jugadas.
+% Resul lista de listas que contiene la mejor jugada que se encuentra a partir de R.
 
-caminoPorNivel2(Grid, Colors, AdyacentesC, Secuencia, Resultado):-
-    findall([Long1,NewGrid, NewSecuencia, NewAdyacentesC], (member(C, Colors), flick(Grid, 
-    C,AdyacentesC, NewGrid, NewAdyacentesC), 
+
+greedyAll(R, 0, R):-!.
+greedyAll(R, N,Resul):-
+    R=[ _LongitudAdy, NewGrid, NewSecuencia, NewAdyacentesC],
+    greedyOne(NewGrid, NewAdyacentesC, N,  NewSecuencia, Resul).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  caminoPorNivel(+Grid, +Colors, +AdyacentesC, +Secuencia, -Resultado)
+%
+% Grid es una grilla.
+% Colors es una lista de colores.
+% AdyacentesC es la lista de celdas capturadas en la grilla Grid.
+% Secuencia es una lista de colores.
+% Resultado es una lista de listas, donde cada una de ellas almacena la cantidad de celdas % capturadas, la longitud de la lista de celdas capturadas, la grilla luego de hacer flick de un % color, la secuencia de colores actualizada y la nueva cantidad de celdas capturadas
+
+caminoPorNivelAlternativo(Grid, Colors, AdyacentesC, Secuencia, Resultado):-
+    findall([Long1,NewGrid, NewSecuencia, NewAdyacentesC], (member(C, Colors), flick(Grid,
+    C,AdyacentesC, NewGrid, NewAdyacentesC),
     length(NewAdyacentesC,Long1),length(AdyacentesC,Long2),
     Long1 > Long2,
-	append(Secuencia, [C], NewSecuencia)),
+    append(Secuencia, [C], NewSecuencia)),
     Resultado), !.
+
